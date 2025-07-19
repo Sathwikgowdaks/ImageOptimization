fd3642f7df49d875bbf4e9792549d68892c726b7
sathwik <sathwik@sathwiks-MacBook-Air-2.local>
2025-07-20 00:01:37 +0530

    Initial commit

---
+++ models/Video.tsx
@@ -0,0 +1,29 @@
+ import mongoose,{Schema,model} from "mongoose";
+
+export const Video_Dimensions = {
+    width: 1080,
+    height: 1920
+}
+export interface IVideo{
+    _id?: mongoose.Types.ObjectId;
+    title: string;
+    description: string;
+    url: string;
+    thumbnailUrl: string;
+    controls?: boolean;
+    transformation?: {
+        width?: number;
+        height?: number;
+        quality?: number;
+    }
+}
+const videoSchema = new Schema<IVideo>({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    url: { type: String, required: true },
+    thumbnailUrl: { type: String, required: true },
+});
+
+const Video = model<IVideo>('Video', videoSchema);
+
+export default Video;
