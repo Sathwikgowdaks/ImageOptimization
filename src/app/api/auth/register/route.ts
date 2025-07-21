c25e28c89de52c89a4953cf53648438b7c3209fc
sathwik <sathwik@sathwiks-MacBook-Air-2.local>
2025-07-21 20:50:49 +0530

    Updated the route file

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,35 @@
+import { connectToDatabase } from "../../../../../lib/db";
+import User from "../../../../../models/User"
+import { NextRequest,NextResponse } from "next/server";
+
+export async function POST(request:NextRequest){
+    try{
+        const {name,email,password}= await request.json();
+        if(!email || !password){
+            return NextResponse.json(
+                {error:"Email and Password are required"},
+                {status:400}
+            )
+        }
+        await connectToDatabase();
+        const existingUser = await User.findOne({email})
+        if(existingUser){
+            return NextResponse.json(
+                {error:"User already registered"},
+                {status:400}
+            )
+        }
+        User.create({
+            name,
+            email,
+            password
+        })
+        return NextResponse.json(
+            {message:"User registered successfully"},
+            {status:400}
+        )
+    }
+    catch{
+
+    }
+}
